4fafefa143d4b356136501513b2459357ebe05fe
Danikileitor <focusyi@hotmail.com>
2024-06-13 18:01:29 +0200

    subida inicial

--- src/main/java/dnk/enchantments/DNKEnchantments.java
+++ src/main/java/dnk/enchantments/DNKEnchantments.java
@@ -28,8 +28,8 @@ import net.minecraftforge.registries.RegistryObject;
 import org.slf4j.Logger;
 
 // The value here should match an entry in the META-INF/mods.toml file
-@Mod(ExampleMod.MODID)
-public class ExampleMod
+@Mod(DNKEnchantments.MODID)
+public class DNKEnchantments
 {
     // Define mod id in a common place for everything to reference
     public static final String MODID = "examplemod";
@@ -59,7 +59,7 @@ public class ExampleMod
                 output.accept(EXAMPLE_ITEM.get()); // Add the example item to the tab. For your own tabs, this method is preferred over the event
             }).build());
 
-    public ExampleMod()
+    public DNKEnchantments()
     {
         IEventBus modEventBus = FMLJavaModLoadingContext.get().getModEventBus();
 

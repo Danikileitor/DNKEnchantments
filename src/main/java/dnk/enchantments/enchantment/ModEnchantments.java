5ac8e31a22ed1f364a47d89e0b002a5cd3cee39f
Danikileitor <focusyi@hotmail.com>
2024-06-16 15:04:29 +0200

    Config

--- src/main/java/dnk/enchantments/enchantment/ModEnchantments.java
+++ src/main/java/dnk/enchantments/enchantment/ModEnchantments.java
@@ -1,7 +1,6 @@
 package dnk.enchantments.enchantment;
 
 import dnk.enchantments.DNKEnchantments;
-import dnk.enchantments.config.CommonConfigs;
 import net.minecraft.world.item.enchantment.Enchantment;
 import net.minecraftforge.eventbus.api.IEventBus;
 import net.minecraftforge.registries.DeferredRegister;
@@ -17,11 +16,6 @@ public class ModEnchantments {
     public static final RegistryObject<Enchantment> MAGNET = ENCHANTMENTS.register("magnet", Jump::new);
 
     public static void register(IEventBus modEventBus) {
-        if (!CommonConfigs.ENCHANTMENT_NIGHTVISION.get()) modEventBus.unregister(NIGHTVISION);
-        if (!CommonConfigs.ENCHANTMENT_LEVITATION.get()) modEventBus.unregister(LEVITATION);
-        if (!CommonConfigs.ENCHANTMENT_JUMP.get()) modEventBus.unregister(JUMP);
-        if (!CommonConfigs.ENCHANTMENT_MAGNET.get()) modEventBus.unregister(MAGNET);
-
         ENCHANTMENTS.register(modEventBus);
     }
 }
